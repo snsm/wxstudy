87366ac369092b354e7fac83774b7646278efde4
陈小龙 <13480731740@qq.com>
2018-04-17 20:51:06 +0800

    学习小程序

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+Page({
+  data: {
+    
+  },
+  //事件处理函数
+  bindViewTap: function() {
+   
+  },
+  onLoad: function () {
+    
+  },
+  shareTime: function() {
+    wx.switchTab({
+      url: '../home/home',
+    });
+  }
+})
